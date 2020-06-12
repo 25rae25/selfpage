9375a19aa9d94eef635d11171182475e3b40213b
25rae25 <rae0024@naver.com>
2020-06-12 16:46:19 +0900

    dd

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/******************* header hover ************************/  
 function onNaviHover() {
 	$(this).find(".ti-mo").stop().fadeIn(500);
 }
@@ -8,7 +9,7 @@ function onNaviLeave() {
 
 $(".titles").hover(onNaviHover, onNaviLeave);
 
-
+/******************* 마지막사진 ************************/  
 $(".end-pic").each(function(i){
 	$(this).css("left", (i* 16.6666)+"%");
 	var wid = $(".end-wrap").innerWidth()/6;
@@ -22,5 +23,39 @@ function onEndHover() {
 	$(this).find("")	
 }
 
+function onEndLeave() {
+
+};
+
+/******************* X자 ************************/  
+$(".bar-wrap .bars").click(onBarClick);
+
+function onBarClick() {
+	if($(this).hasClass("active")) {
+		$(this).removeClass("active");
+		$(this).find(".bar1").css("animation-name", "bar1-rev");
+		$(this).find(".bar2").css("animation-name", "bar2-rev");
+		$(this).find(".bar3").css("animation-name", "bar3-rev");
+	}
+	else {
+		$(this).addClass("active");
+		$(this).find(".bar1").css("animation-name", "bar1");
+		$(this).find(".bar2").css("animation-name", "bar2");
+		$(this).find(".bar3").css("animation-name", "bar3");
+	}
+}
+
+
+/******************* main사진 ************************/  
+var mainSlide = new Slide({
+	type: 'scale', 
+	container: '.picture', 
+	slide: '.slide',
+	before: function(prev, next, container){
+
+	},
+	complete: function(prev, next, container){
+
+	}
 
 
