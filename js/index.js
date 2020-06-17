a0eb24a3be9517cf244288d91dc0f6d0f96889c7
booldook <booldook@gmail.com>
2020-06-17 15:54:51 +0900

    booldook

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-/******************* header hover ************************/  
+/******************* header hover ************************/
 function onNaviHover() {
 	$(this).find(".ti-mo").stop().fadeIn(500);
 }
@@ -9,29 +9,29 @@ function onNaviLeave() {
 
 $(".titles").hover(onNaviHover, onNaviLeave);
 
-/******************* 마지막사진 ************************/  
-$(".end-pic").each(function(i){
-	$(this).css("left", (i* 16.6666)+"%");
-	var wid = $(".end-wrap").innerWidth()/6;
+/******************* 마지막사진 ************************/
+$(".end-pic").each(function (i) {
+	$(this).css("left", (i * 16.6666) + "%");
+	var wid = $(".end-wrap").innerWidth() / 6;
 	$(this).find(".end-cont").innerWidth(wid);
 });
 
 $(".end-pic").hover(onEndHover, onEndLeave);
 
-/* function onEndHover() {
-	$(this).css("left", (i* 100)+"%");
-	$(this).find("")	
+function onEndHover() {
+	$(this).css("left", (i * 100) + "%");
+	$(this).find("")
 }
 
 function onEndLeave() {
 
-}; */
+};
 
-/******************* X자 ************************/  
+/******************* X자 ************************/
 $(".bar-wrap .bars").click(onBarClick);
 
 function onBarClick() {
-	if($(this).hasClass("active")) {
+	if ($(this).hasClass("active")) {
 		$(this).removeClass("active");
 		$(this).find(".bar1").css("animation-name", "bar1-rev");
 		$(this).find(".bar2").css("animation-name", "bar2-rev");
@@ -46,12 +46,12 @@ function onBarClick() {
 }
 
 
-/******************* pic ************************/  
+/******************* pic ************************/
 /* var slide = new Slide(".picture", ".pic", "scale"); */
 
 var picNow = 0;
 var picSlide = $(".picture > .pic");
-var picLast = picSlide.length -1;
+var picLast = picSlide.length - 1;
 
 picInit();
 
@@ -60,27 +60,14 @@ function picInit() {
 	$(picSlide[picNow]).appendTo(".picture");
 }
 
-function picAni() {
-	var picSlide = $(picSlide[picNow]).appendTo(".picture").css({"transform":"scale(1.3)",
-"opacity":0});
+function onInterval() {
+	picNow = (picNow == picLast) ? 0 : picNow + 1;
+	$(picSlide[picNow]).appendTo(".picture").css("opacity", 0).stop().animate({"opacity": 1}, 500);
 }
-setTimeout(function(){
-	$(".picture").find(".pic1").css({"transform":"scale(1.3)","opacity":0})
-	$(".picture").find(".pic2").css({"transform":"scale(0.8)","opacity":0})
-	$(".picture").find(".pic3").css({"transform":"scale(1.3)","opacity":0})
 
-});
+setInterval(onInterval, 3000);
 
 
-function onpicPrev() {
 
-picNow = (picNow == 0) ? picLast : picNow - 1;
-mainAni();
-}
-function onpicNext() {
 
-picNow = (picNow == picLast) ? 0 : picNow + 1;
-picAni();
-}
-$(".picture > .bt-prev").click(onMainPrev);
-$(".picture > .bt-next").click(onMainNext);
+
